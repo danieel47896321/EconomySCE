3126db8665bbbf66889d8ea8e2ed643174a41853
Daniel <danieel47896321@gmail.com>
2024-02-14 10:50:15 +0200

    Fixed strings at CalculationAdapter.

--- app/src/main/java/com/example/economysce/Adapters/CalculationAdapter.java
+++ app/src/main/java/com/example/economysce/Adapters/CalculationAdapter.java
@@ -33,18 +33,18 @@ public class CalculationAdapter extends RecyclerView.Adapter<CalculationAdapter.
         holder.Id.setText(employeeCalculation.getId() + ".");
         holder.FirstName.setText(context.getResources().getString(R.string.FirstName) + employeeCalculation.getName() + ", ");
         holder.LastName.setText(context.getResources().getString(R.string.LastName) + employeeCalculation.getLastName());
-        holder.Compensation.setText("סכום הפיצויים: "  + String.format("%.02f",employeeCalculation.getCompensation()));
-        holder.CostOfOnGoingService.setText("עלות שירות שוטף: " + String.format("%.02f",employeeCalculation.getOngoingServiceCost()));
-        holder.CostOfServiceExpectation.setText("עלות יוון שוטף: " + String.format("%.02f",employeeCalculation.getDiscountCost()));
+        holder.Compensation.setText(context.getResources().getString(R.string.Compensation) + " " + String.format("%.02f",employeeCalculation.getCompensation()));
+        holder.CostOfOnGoingService.setText(context.getResources().getString(R.string.CostOfOnGoingService) + " " + String.format("%.02f",employeeCalculation.getOngoingServiceCost()));
+        holder.CostOfServiceExpectation.setText(context.getResources().getString(R.string.CostOfServiceExpectation) + " " + String.format("%.02f",employeeCalculation.getDiscountCost()));
         if(employeeCalculation.getActuarialLossGainInLiability() < 0)
-            holder.ActuarialLossGainInLiability.setText("הפסד/רווח אקטוארי בהתחייבות: " + String.format("%.02f",Math.abs(employeeCalculation.getActuarialLossGainInLiability())) + "-");
+            holder.ActuarialLossGainInLiability.setText(context.getResources().getString(R.string.ActuarialLossGainInLiability) + " " + String.format("%.02f",Math.abs(employeeCalculation.getActuarialLossGainInLiability())) + "-");
         else
-            holder.ActuarialLossGainInLiability.setText("הפסד/רווח אקטוארי בהתחייבות: " + String.format("%.02f",employeeCalculation.getActuarialLossGainInLiability()));
-        holder.ExpectedAssetsReturns.setText("תשואה צפויה על נכסי התוכנית: " + String.format("%.02f",employeeCalculation.getExpectedAssetsReturns()));
+            holder.ActuarialLossGainInLiability.setText(context.getResources().getString(R.string.ActuarialLossGainInLiability) + " " + String.format("%.02f",employeeCalculation.getActuarialLossGainInLiability()));
+        holder.ExpectedAssetsReturns.setText(context.getResources().getString(R.string.ExpectedAssetsReturns) + " " + String.format("%.02f",employeeCalculation.getExpectedAssetsReturns()));
         if(employeeCalculation.getActuarialLossGainInAssets() < 0)
-            holder.ActuarialLossGainInAssets.setText("רווחים/הפסדים אקטוארים נכסים: " + String.format("%.02f",Math.abs(employeeCalculation.getActuarialLossGainInAssets())) + "-");
+            holder.ActuarialLossGainInAssets.setText(context.getResources().getString(R.string.ActuarialLossGainInAssets) + " " + String.format("%.02f",Math.abs(employeeCalculation.getActuarialLossGainInAssets())) + "-");
         else
-            holder.ActuarialLossGainInAssets.setText("רווחים/הפסדים אקטוארים נכסים: " + String.format("%.02f",employeeCalculation.getActuarialLossGainInAssets()));
+            holder.ActuarialLossGainInAssets.setText(context.getResources().getString(R.string.ActuarialLossGainInAssets) + " " + String.format("%.02f",employeeCalculation.getActuarialLossGainInAssets()));
     }
     @Override
     public int getItemCount() { return employeeCalculationList.size(); }
